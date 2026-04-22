b45fa0cdee4486cd46c0a2bc4c204ef61e63499b
IslandAdventurer <damajosue12@gmail.com>
2026-04-21 22:39:49 -0400

    Updated PollServe to include the poll manager

--- PollServer.java
+++ PollServer.java
@@ -3,12 +3,16 @@ import java.io.*;
 
 public class PollServer {
     public static void main(String[] args) {
+        PollManager manager = new PollManager()
         try {
             ServerSocket serverSocket = new ServerSocket(9001);
-            System.out.println("The server on port 9001 started.");
+            System.out.println("The server started.");
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("The client is connected.");
+                System.out.println("The client connected.");
+                ClientHandler handler = new ClientHandler)clientSocket, manager);
+                Thread thread = new Thread(handler);
+                thread.start();
             }
         
         } catch (IOException e) {
